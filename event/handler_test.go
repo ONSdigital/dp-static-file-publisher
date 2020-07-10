23324f42c666f7929fce7e09ed04f297a91e99c5
David Subiros <subiros.david@gmail.com>
2020-07-10 11:12:16 +0100

    Implemented negative test cases for event handler

--- event/handler_test.go
+++ event/handler_test.go
@@ -3,6 +3,7 @@ package event_test
 import (
 	"bytes"
 	"encoding/hex"
+	"errors"
 	"io"
 	"io/ioutil"
 	"testing"
@@ -24,47 +25,46 @@ var (
 	fileBytes         []byte        = []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	testFileContent   io.ReadCloser = ioutil.NopCloser(bytes.NewReader(fileBytes))
 	encodedPSK        string        = "48656C6C6F20576F726C64"
+	errVault                        = errors.New("Vault error")
+	errS3Private                    = errors.New("S3Private error")
+	errS3Public                     = errors.New("S3Public error")
 )
 
 func TestDataImportCompleteHandler_Handle_HierarchyStoreError(t *testing.T) {
 
-	Convey("Given an event handler with successful mock S3 and Vault clients", t, func() {
+	Convey("Given S3 and Vault client mocks", t, func() {
 
 		mockS3Private := &mock.S3ClientMock{
 			BucketNameFunc: func() string {
 				return testPrivateBucket
 			},
-			GetWithPSKFunc: func(key string, psk []byte) (io.ReadCloser, *int64, error) {
-				return testFileContent, &testSize, nil
-			},
 		}
-
 		mockS3Public := &mock.S3UploaderMock{
 			BucketNameFunc: func() string {
 				return testPublicBucket
 			},
-			UploadFunc: func(input *s3manager.UploadInput, options ...func(*s3manager.Uploader)) (*s3manager.UploadOutput, error) {
-				return &s3manager.UploadOutput{}, nil
-			},
 		}
-
 		mockVault := &mock.VaultClientMock{
 			ReadKeyFunc: func(path string, key string) (string, error) {
 				return encodedPSK, nil
 			},
 		}
-
-		eventHandler := event.ImagePublishedHandler{
-			S3Public:  mockS3Public,
-			S3Private: mockS3Private,
-			VaultCli:  mockVault,
-			VaultPath: testVaultPath,
-		}
-
 		psk, err := hex.DecodeString(encodedPSK)
 		So(err, ShouldBeNil)
 
-		Convey("When Handle is triggered", func() {
+		Convey("And a successful event handler, when Handle is triggered", func() {
+			mockS3Private.GetWithPSKFunc = func(key string, psk []byte) (io.ReadCloser, *int64, error) {
+				return testFileContent, &testSize, nil
+			}
+			mockS3Public.UploadFunc = func(input *s3manager.UploadInput, options ...func(*s3manager.Uploader)) (*s3manager.UploadOutput, error) {
+				return &s3manager.UploadOutput{}, nil
+			}
+			eventHandler := event.ImagePublishedHandler{
+				S3Public:  mockS3Public,
+				S3Private: mockS3Private,
+				VaultCli:  mockVault,
+				VaultPath: testVaultPath,
+			}
 			err := eventHandler.Handle(testCtx, &testEvent)
 			So(err, ShouldBeNil)
 
@@ -89,7 +89,88 @@ func TestDataImportCompleteHandler_Handle_HierarchyStoreError(t *testing.T) {
 				})
 			})
 		})
+
+		Convey("And an event handler with a failing vault client, when Handle is triggered", func() {
+			mockVaultFail := &mock.VaultClientMock{
+				ReadKeyFunc: func(path string, key string) (string, error) {
+					return "", errVault
+				},
+			}
+			eventHandler := event.ImagePublishedHandler{
+				S3Public:  mockS3Public,
+				S3Private: mockS3Private,
+				VaultCli:  mockVaultFail,
+				VaultPath: testVaultPath,
+			}
+			err := eventHandler.Handle(testCtx, &testEvent)
+
+			Convey("Vault ReadKey is called and the error is returned", func() {
+				So(err, ShouldResemble, errVault)
+				So(len(mockVaultFail.ReadKeyCalls()), ShouldEqual, 1)
+			})
+		})
+
+		Convey("And an event handler with a vault client that returns an invalid psk, when Handle is triggered", func() {
+			mockVaultFail := &mock.VaultClientMock{
+				ReadKeyFunc: func(path string, key string) (string, error) {
+					return "invalidPSK", nil
+				},
+			}
+			eventHandler := event.ImagePublishedHandler{
+				S3Public:  mockS3Public,
+				S3Private: mockS3Private,
+				VaultCli:  mockVaultFail,
+				VaultPath: testVaultPath,
+			}
+			err := eventHandler.Handle(testCtx, &testEvent)
+
+			Convey("Vault ReadKey is called and the decoding error is returned", func() {
+				So(err, ShouldNotBeNil)
+				So(len(mockVaultFail.ReadKeyCalls()), ShouldEqual, 1)
+			})
+		})
+
+		Convey("And an event handler with an S3Private client that fails to obtain the source file, when Handle is triggered", func() {
+			mockS3Private.GetWithPSKFunc = func(key string, psk []byte) (io.ReadCloser, *int64, error) {
+				return nil, nil, errS3Private
+			}
+			eventHandler := event.ImagePublishedHandler{
+				S3Public:  mockS3Public,
+				S3Private: mockS3Private,
+				VaultCli:  mockVault,
+				VaultPath: testVaultPath,
+			}
+			err := eventHandler.Handle(testCtx, &testEvent)
+
+			Convey("S3Private is called and the same error is returned", func() {
+				So(err, ShouldResemble, errS3Private)
+				So(len(mockVault.ReadKeyCalls()), ShouldEqual, 1)
+				So(len(mockS3Private.GetWithPSKCalls()), ShouldEqual, 1)
+			})
+		})
+
+		Convey("And an event handler with an S3Public client that fails to upload the file, when Handle is triggered", func() {
+			mockS3Private.GetWithPSKFunc = func(key string, psk []byte) (io.ReadCloser, *int64, error) {
+				return testFileContent, &testSize, nil
+			}
+			mockS3Public.UploadFunc = func(input *s3manager.UploadInput, options ...func(*s3manager.Uploader)) (*s3manager.UploadOutput, error) {
+				return nil, errS3Public
+			}
+			eventHandler := event.ImagePublishedHandler{
+				S3Public:  mockS3Public,
+				S3Private: mockS3Private,
+				VaultCli:  mockVault,
+				VaultPath: testVaultPath,
+			}
+			err := eventHandler.Handle(testCtx, &testEvent)
+
+			Convey("S3Private is called and the same error is returned", func() {
+				So(err, ShouldResemble, errS3Public)
+				So(len(mockVault.ReadKeyCalls()), ShouldEqual, 1)
+				So(len(mockS3Private.GetWithPSKCalls()), ShouldEqual, 1)
+				So(len(mockS3Public.BucketNameCalls()), ShouldEqual, 1)
+			})
+		})
 	})
 
-	// TODO implement negative path tests
 }
