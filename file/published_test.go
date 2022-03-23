bb722a25131f054eecc81eba7e1bdb75037a00d9
Mark Bradley <braddle@gmail.com>
2022-03-23 15:48:00 +0000

    Refactoring tests: removing duplication with table test, generator function and moving duplicated function to variables

--- file/published_test.go
+++ file/published_test.go
@@ -19,349 +19,173 @@ import (
 	"testing"
 )
 
-const errMsg = "s3 is broken"
+const (
+	errMsg    = "s3 is broken"
+	vaultPath = "testing/secret"
+)
+
+var (
+	s3Client    *fileMock.S3ClientV2Mock
+	vaultClient *eventMock.VaultClientMock
+
+	nopCloser            = io.NopCloser(strings.NewReader("testing"))
+	validVaultReadFunc   = func(path string, key string) (string, error) { return "1234567890123456", nil }
+	fileDoesNotExistFunc = func(key string) (bool, error) { return false, nil }
+	validGetWithPSKFunc  = func(key string, psk []byte) (io.ReadCloser, *int64, error) { return nopCloser, nil, nil }
+	validUploadFunc      = func(input *s3manager.UploadInput, options ...func(*s3manager.Uploader)) (*s3manager.UploadOutput, error) {
+		return &s3manager.UploadOutput{ETag: aws.String("1234567890")}, nil
+	}
+)
 
 func TestHandleFilePublishMessage(t *testing.T) {
-	schema := &avro.Schema{
-		Definition: `{
-					"type": "record",
-					"name": "file-published",
-					"fields": [
-					  {"name": "path", "type": "string"},
-					  {"name": "etag", "type": "string"},
-					  {"name": "type", "type": "string"},
-					  {"name": "sizeInBytes", "type": "string"}
-					]
-				  }`,
-	}
-	fp := file.Published{
-		Path:        "test/file.txt",
-		Type:        "plain/test",
-		Etag:        "1234567890",
-		SizeInBytes: "123",
-	}
-
-	c, _ := schema.Marshal(fp)
-
-	msg := MockMessage{
-		Data: c,
-	}
 	ctx := context.Background()
 
-	pc := &fileMock.S3ClientV2Mock{
-		FileExistsFunc: func(key string) (bool, error) {
-			return false, nil
-		},
+	s3Client = &fileMock.S3ClientV2Mock{
+		FileExistsFunc: fileDoesNotExistFunc,
 	}
+	vaultClient = &eventMock.VaultClientMock{}
 
 	Convey("Given invalid message content", t, func() {
-		dc := file.DecrypterCopier{}
+
+		msg := MockMessage{}
 		msg.Data = []byte("Testing")
 
 		Convey("When the message is handled", func() {
-			err := dc.HandleFilePublishMessage(ctx, 1, msg)
+			err := generateDecrypterCopier("does not matter").HandleFilePublishMessage(ctx, 1, msg)
 
-			So(err, ShouldBeError)
-
-			commiter, ok := err.(kafka.Commiter)
-
-			So(ok, ShouldBeTrue)
-			So(commiter.Commit(), ShouldBeFalse)
+			Convey("Then a No Commit error should be returned", func() {
+				So(err, ShouldBeError)
+				ensureNoCommitError(err)
+			})
 		})
 	})
 
 	Convey("Given there is a read error on the Vault key", t, func() {
-		vc := &eventMock.VaultClientMock{ReadKeyFunc: func(path string, key string) (string, error) {
-			return "", errors.New("broken")
-		}}
-
-		dc := file.DecrypterCopier{
-			VaultClient:  vc,
-			PublicClient: pc,
-		}
+		vaultClient.ReadKeyFunc = func(path string, key string) (string, error) { return "", errors.New("broken") }
 
 		Convey("When the message is handled", func() {
-			err := dc.HandleFilePublishMessage(ctx, 1, msg)
+			err := generateDecrypterCopier("does not matter").HandleFilePublishMessage(ctx, 1, generateMockMessage())
 
-			So(err, ShouldBeError)
-			commiter, ok := err.(kafka.Commiter)
-
-			So(ok, ShouldBeTrue)
-			So(commiter.Commit(), ShouldBeFalse)
+			Convey("Then a No Commit error should be returned", func() {
+				So(err, ShouldBeError)
+				ensureNoCommitError(err)
+			})
 		})
 	})
 
 	Convey("Given there a specific vault read error", t, func() {
-		vc := &eventMock.VaultClientMock{}
-
-		dc := file.DecrypterCopier{
-			VaultClient:  vc,
-			PublicClient: pc,
+		var errorScenarios = []struct {
+			err     error
+			errName string
+		}{
+			{vault.ErrKeyNotFound, "ErrKeyNotFound"},
+			{vault.ErrVersionNotFound, "ErrVersionNotFound"},
+			{vault.ErrMetadataNotFound, "ErrMetadataNotFound"},
+			{vault.ErrDataNotFound, "ErrDataNotFound"},
+			{vault.ErrVersionInvalid, "ErrVersionInvalid"},
 		}
 
-		msg.Data = c
+		for _, scenario := range errorScenarios {
+			Convey("And the error is "+scenario.errName, func() {
+				vaultClient.ReadKeyFunc = func(path string, key string) (string, error) { return "", scenario.err }
 
-		Convey("When the error is ErrKeyNotFound", func() {
-			vc.ReadKeyFunc = func(path string, key string) (string, error) {
-				return "", vault.ErrKeyNotFound
-			}
+				Convey("When the message is handled", func() {
+					err := generateDecrypterCopier("does not matter").HandleFilePublishMessage(ctx, 1, generateMockMessage())
 
-			err := dc.HandleFilePublishMessage(ctx, 1, msg)
-
-			So(err, ShouldBeError)
-			So(err.Error(), ShouldEqual, vault.ErrKeyNotFound.Error())
-			commiter, ok := err.(kafka.Commiter)
-
-			So(ok, ShouldBeTrue)
-			So(commiter.Commit(), ShouldBeFalse)
-		})
-
-		Convey("When the error is ErrVersionNotFound", func() {
-			vc.ReadKeyFunc = func(path string, key string) (string, error) {
-				return "", vault.ErrVersionNotFound
-			}
-
-			err := dc.HandleFilePublishMessage(ctx, 1, msg)
-
-			So(err, ShouldBeError)
-			So(err.Error(), ShouldEqual, vault.ErrVersionNotFound.Error())
-			commiter, ok := err.(kafka.Commiter)
-
-			So(ok, ShouldBeTrue)
-			So(commiter.Commit(), ShouldBeFalse)
-		})
-
-		Convey("When the error is ErrMetadataNotFound", func() {
-			vc.ReadKeyFunc = func(path string, key string) (string, error) {
-				return "", vault.ErrMetadataNotFound
-			}
-
-			err := dc.HandleFilePublishMessage(ctx, 1, msg)
-
-			So(err, ShouldBeError)
-			So(err.Error(), ShouldEqual, vault.ErrMetadataNotFound.Error())
-			commiter, ok := err.(kafka.Commiter)
-
-			So(ok, ShouldBeTrue)
-			So(commiter.Commit(), ShouldBeFalse)
-		})
-
-		Convey("When the error is ErrDataNotFound", func() {
-			vc.ReadKeyFunc = func(path string, key string) (string, error) {
-				return "", vault.ErrDataNotFound
-			}
-
-			err := dc.HandleFilePublishMessage(ctx, 1, msg)
-
-			So(err, ShouldBeError)
-			So(err.Error(), ShouldEqual, vault.ErrDataNotFound.Error())
-			commiter, ok := err.(kafka.Commiter)
-
-			So(ok, ShouldBeTrue)
-			So(commiter.Commit(), ShouldBeFalse)
-		})
-
-		Convey("When the error is ErrVersionInvalid", func() {
-			vc.ReadKeyFunc = func(path string, key string) (string, error) {
-				return "", vault.ErrVersionInvalid
-			}
-
-			err := dc.HandleFilePublishMessage(ctx, 1, msg)
-
-			So(err, ShouldBeError)
-			So(err.Error(), ShouldEqual, vault.ErrVersionInvalid.Error())
-			commiter, ok := err.(kafka.Commiter)
-
-			So(ok, ShouldBeTrue)
-			So(commiter.Commit(), ShouldBeFalse)
-		})
+					Convey("Then a No Commit error should be returned wrapping the original error", func() {
+						ensureNoCommitErrorWithMessage(err, scenario.err.Error())
+					})
+				})
+			})
+		}
 	})
 
-	Convey("Given the encryption key from vault cannot be parse to a byte array", t, func() {
-		vc := &eventMock.VaultClientMock{}
+	Convey("Given the encryption key from vault cannot be parsed to a byte array", t, func() {
+		vaultClient.ReadKeyFunc = func(path string, key string) (string, error) { return "abcdefgh", nil }
 
-		dc := file.DecrypterCopier{
-			VaultClient:  vc,
-			PublicClient: pc,
-		}
-		msg := MockMessage{
-			Data: c,
-		}
+		Convey("When the message is handled", func() {
+			err := generateDecrypterCopier("does not matter").HandleFilePublishMessage(ctx, 1, generateMockMessage())
 
-		Convey("When the encryption key is abcdefgh", func() {
-			vc.ReadKeyFunc = func(path string, key string) (string, error) {
-				return "abcdefgh", nil
-			}
-
-			err := dc.HandleFilePublishMessage(ctx, 1, msg)
-
-			So(err, ShouldBeError)
-			So(err.Error(), ShouldContainSubstring, "encoding/hex:")
-
-			commiter, ok := err.(kafka.Commiter)
-
-			So(ok, ShouldBeTrue)
-			So(commiter.Commit(), ShouldBeFalse)
+			Convey("Then a No Commit error should be returned", func() {
+				ensureNoCommitErrorWithPartMessage(err, "encoding/hex:")
+			})
 		})
 	})
 
 	Convey("Given files in the private bucket could not be read", t, func() {
-		vc := &eventMock.VaultClientMock{
-			ReadKeyFunc: func(path string, key string) (string, error) {
-				return "1234567890123456", nil
-			},
-		}
+		vaultClient.ReadKeyFunc = validVaultReadFunc
 
-		pc.GetWithPSKFunc = func(key string, psk []byte) (io.ReadCloser, *int64, error) { return nil, nil, errors.New(errMsg) }
-		pc.FileExistsFunc = func(key string) (bool, error) { return false, nil }
-
-		dc := file.DecrypterCopier{
-			VaultClient:   vc,
-			PrivateClient: pc,
-			PublicClient:  pc,
-		}
+		s3Client.GetWithPSKFunc = func(key string, psk []byte) (io.ReadCloser, *int64, error) { return nil, nil, errors.New(errMsg) }
+		s3Client.FileExistsFunc = fileDoesNotExistFunc
 
 		Convey("Attempting to get a file from to  private s3 bucket", func() {
-			err := dc.HandleFilePublishMessage(ctx, 1, msg)
+			err := generateDecrypterCopier("does not matter").HandleFilePublishMessage(ctx, 1, generateMockMessage())
 
-			So(err, ShouldBeError)
-			So(err.Error(), ShouldEqual, errMsg)
-			commiter, ok := err.(kafka.Commiter)
-
-			So(ok, ShouldBeTrue)
-			So(commiter.Commit(), ShouldBeFalse)
+			Convey("Then a No Commit error should be returned", func() {
+				ensureNoCommitErrorWithMessage(err, errMsg)
+			})
 		})
 	})
 
 	Convey("Given files sent the public bucket could not be written", t, func() {
-		vc := &eventMock.VaultClientMock{
-			ReadKeyFunc: func(path string, key string) (string, error) {
-				return "1234567890123456", nil
-			},
-		}
+		vaultClient.ReadKeyFunc = validVaultReadFunc
 
-		pc.GetWithPSKFunc = func(key string, psk []byte) (io.ReadCloser, *int64, error) {
-			return io.NopCloser(strings.NewReader("testing")), nil, nil
-		}
-		pc.UploadFunc = func(input *s3manager.UploadInput, options ...func(*s3manager.Uploader)) (*s3manager.UploadOutput, error) {
+		s3Client.GetWithPSKFunc = validGetWithPSKFunc
+		s3Client.UploadFunc = func(input *s3manager.UploadInput, options ...func(*s3manager.Uploader)) (*s3manager.UploadOutput, error) {
 			return nil, errors.New(errMsg)
 		}
 
-		dc := file.DecrypterCopier{
-			VaultClient:   vc,
-			PrivateClient: pc,
-			PublicClient:  pc,
-		}
-
 		Convey("Attempting to get a file from to  private s3 bucket", func() {
-			err := dc.HandleFilePublishMessage(ctx, 1, msg)
+			err := generateDecrypterCopier("does not matter").HandleFilePublishMessage(ctx, 1, generateMockMessage())
 
-			So(err, ShouldBeError)
-			So(err.Error(), ShouldEqual, errMsg)
-			commiter, ok := err.(kafka.Commiter)
-
-			So(ok, ShouldBeTrue)
-			So(commiter.Commit(), ShouldBeFalse)
+			Convey("Then a No Commit error should be returned", func() {
+				ensureNoCommitErrorWithMessage(err, errMsg)
+			})
 		})
 	})
 
 	Convey("Given there already is a file in the public bucket with the provided path", t, func() {
-		vc := &eventMock.VaultClientMock{
-			ReadKeyFunc: func(path string, key string) (string, error) {
-				return "1234567890123456", nil
-			},
-		}
+		vaultClient.ReadKeyFunc = validVaultReadFunc
 
-		pc.GetWithPSKFunc = func(key string, psk []byte) (io.ReadCloser, *int64, error) {
-			return io.NopCloser(strings.NewReader("testing")), nil, nil
-		}
-		pc.FileExistsFunc = func(key string) (bool, error) {
-			return true, nil
-		}
-
-		dc := file.DecrypterCopier{
-			VaultClient:   vc,
-			PrivateClient: pc,
-			PublicClient:  pc,
-		}
+		s3Client.GetWithPSKFunc = validGetWithPSKFunc
+		s3Client.FileExistsFunc = func(key string) (bool, error) { return true, nil }
 
 		Convey("When the duplicate file is sent for decryption", func() {
-			err := dc.HandleFilePublishMessage(ctx, 1, msg)
+			err := generateDecrypterCopier("does not matter").HandleFilePublishMessage(ctx, 1, generateMockMessage())
 
-			So(err, ShouldBeError)
-			So(err.Error(), ShouldContainSubstring, "decrypted file already exists")
-			commiter, ok := err.(kafka.Commiter)
-
-			So(ok, ShouldBeTrue)
-			So(commiter.Commit(), ShouldBeFalse)
+			Convey("Then a No Commit error should be returned", func() {
+				ensureNoCommitErrorWithPartMessage(err, "decrypted file already exists")
+			})
 		})
 	})
 
 	Convey("Given there is a error checking the head of the file in the public bucket", t, func() {
-		vc := &eventMock.VaultClientMock{
-			ReadKeyFunc: func(path string, key string) (string, error) {
-				return "1234567890123456", nil
-			},
-		}
+		vaultClient.ReadKeyFunc = validVaultReadFunc
 
-		pc.GetWithPSKFunc = func(key string, psk []byte) (io.ReadCloser, *int64, error) {
-			return io.NopCloser(strings.NewReader("testing")), nil, nil
-		}
-		pc.FileExistsFunc = func(key string) (bool, error) {
-			return false, errors.New(errMsg)
-		}
-
-		dc := file.DecrypterCopier{
-			VaultClient:   vc,
-			PrivateClient: pc,
-			PublicClient:  pc,
-		}
+		s3Client.GetWithPSKFunc = validGetWithPSKFunc
+		s3Client.FileExistsFunc = func(key string) (bool, error) { return false, errors.New(errMsg) }
 
 		Convey("When Head error is returned", func() {
-			err := dc.HandleFilePublishMessage(ctx, 1, msg)
+			err := generateDecrypterCopier("does not matter").HandleFilePublishMessage(ctx, 1, generateMockMessage())
 
-			So(err, ShouldBeError)
-			So(err.Error(), ShouldContainSubstring, errMsg)
-			commiter, ok := err.(kafka.Commiter)
-
-			So(ok, ShouldBeTrue)
-			So(commiter.Commit(), ShouldBeFalse)
+			Convey("Then a No Commit error should be returned", func() {
+				ensureNoCommitErrorWithMessage(err, errMsg)
+			})
 		})
 	})
 
 	Convey("Given the file api hostname is invalid", t, func() {
-		vc := &eventMock.VaultClientMock{
-			ReadKeyFunc: func(path string, key string) (string, error) {
-				return "1234567890123456", nil
-			},
-		}
+		vaultClient.ReadKeyFunc = validVaultReadFunc
 
-		pc.GetWithPSKFunc = func(key string, psk []byte) (io.ReadCloser, *int64, error) {
-			return io.NopCloser(strings.NewReader("testing")), nil, nil
-		}
-		pc.FileExistsFunc = func(key string) (bool, error) {
-			return false, nil
-		}
-		pc.UploadFunc = func(input *s3manager.UploadInput, options ...func(*s3manager.Uploader)) (*s3manager.UploadOutput, error) {
-			return &s3manager.UploadOutput{ETag: aws.String("1234567890")}, nil
-		}
-
-		dc := file.DecrypterCopier{
-			VaultClient:   vc,
-			PrivateClient: pc,
-			PublicClient:  pc,
-			FilesAPIURL:   "completely broken",
-		}
-
+		s3Client.GetWithPSKFunc = validGetWithPSKFunc
+		s3Client.FileExistsFunc = fileDoesNotExistFunc
+		s3Client.UploadFunc = validUploadFunc
 		Convey("When the hostname is invalid", func() {
-			err := dc.HandleFilePublishMessage(ctx, 1, msg)
+			err := generateDecrypterCopier("invalid").HandleFilePublishMessage(ctx, 1, generateMockMessage())
 
-			So(err, ShouldBeError)
-			So(err.Error(), ShouldContainSubstring, "unsupported protocol scheme")
-			commiter, ok := err.(kafka.Commiter)
-
-			So(ok, ShouldBeTrue)
-			So(commiter.Commit(), ShouldBeFalse)
+			Convey("Then a No Commit error should be returned", func() {
+				ensureNoCommitErrorWithPartMessage(err, "unsupported protocol scheme")
+			})
 		})
 	})
 
@@ -412,72 +236,77 @@ func TestHandleFilePublishMessage(t *testing.T) {
 
 		for _, errorScenario := range errorScenarios {
 			Convey(errorScenario.description, func() {
-				vc := &eventMock.VaultClientMock{
-					ReadKeyFunc: func(path string, key string) (string, error) {
-						return "1234567890123456", nil
-					},
-				}
+				vaultClient.ReadKeyFunc = validVaultReadFunc
 
-				pc.GetWithPSKFunc = func(key string, psk []byte) (io.ReadCloser, *int64, error) {
-					return io.NopCloser(strings.NewReader("testing")), nil, nil
-				}
-				pc.FileExistsFunc = func(key string) (bool, error) {
-					return false, nil
-				}
-				pc.UploadFunc = func(input *s3manager.UploadInput, options ...func(*s3manager.Uploader)) (*s3manager.UploadOutput, error) {
-					return &s3manager.UploadOutput{ETag: aws.String("1234567890")}, nil
-				}
+				s3Client.GetWithPSKFunc = validGetWithPSKFunc
+				s3Client.FileExistsFunc = fileDoesNotExistFunc
+				s3Client.UploadFunc = validUploadFunc
 
 				s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 					w.WriteHeader(errorScenario.status)
 				}))
 				defer s.Close()
 
-				dc := file.DecrypterCopier{
-					VaultClient:   vc,
-					PrivateClient: pc,
-					PublicClient:  pc,
-					FilesAPIURL:   s.URL,
-				}
+				err := generateDecrypterCopier(s.URL).HandleFilePublishMessage(ctx, 1, generateMockMessage())
 
-				err := dc.HandleFilePublishMessage(ctx, 1, msg)
-
-				So(err, ShouldBeError)
-				So(err.Error(), ShouldContainSubstring, errorScenario.errorString)
-				commiter, ok := err.(kafka.Commiter)
-
-				So(ok, ShouldBeTrue)
-				So(commiter.Commit(), errorScenario.willCommit)
+				Convey("Then a No Commit error should be returned", func() {
+					ensureNoCommitErrorWithPartMessage(err, errorScenario.errorString)
+				})
 			})
 		}
 	})
+}
+
+func ensureNoCommitErrorWithPartMessage(err error, part string) {
+	So(err, ShouldBeError)
+	So(err.Error(), ShouldContainSubstring, part)
+	ensureNoCommitError(err)
+}
+
+func ensureNoCommitErrorWithMessage(err error, msg string) {
+	So(err, ShouldBeError)
+	So(err.Error(), ShouldEqual, msg)
+	ensureNoCommitError(err)
+}
+
+func ensureNoCommitError(err error) {
+	commiter, ok := err.(kafka.Commiter)
+
+	So(ok, ShouldBeTrue)
+	So(commiter.Commit(), ShouldBeFalse)
+}
+
+func generateDecrypterCopier(fileAPIURL string) file.DecrypterCopier {
+	return file.NewDecrypterCopier(s3Client, s3Client, vaultClient, vaultPath, fileAPIURL)
+}
+
+func generateMockMessage() MockMessage {
+	schema := &avro.Schema{
+		Definition: `{
+					"type": "record",
+					"name": "file-published",
+					"fields": [
+					  {"name": "path", "type": "string"},
+					  {"name": "etag", "type": "string"},
+					  {"name": "type", "type": "string"},
+					  {"name": "sizeInBytes", "type": "string"}
+					]
+				  }`,
+	}
+
+	c, _ := schema.Marshal(file.Published{"test/file.txt", "plain/test", "1234567890", "123"})
+
+	return MockMessage{Data: c}
 }
 
 type MockMessage struct {
 	Data []byte
 }
 
-func (m MockMessage) GetData() []byte {
-	return m.Data
-}
-
-func (m MockMessage) Mark() {
-}
-
-func (m MockMessage) Commit() {
-
-}
-
-func (m MockMessage) Release() {
-}
-
-func (m MockMessage) CommitAndRelease() {
-}
-
-func (m MockMessage) Offset() int64 {
-	return 1
-}
-
-func (m MockMessage) UpstreamDone() chan struct{} {
-	return nil
-}
+func (m MockMessage) GetData() []byte             { return m.Data }
+func (m MockMessage) Offset() int64               { return 1 }
+func (m MockMessage) UpstreamDone() chan struct{} { return nil }
+func (m MockMessage) Mark()                       {}
+func (m MockMessage) Commit()                     {}
+func (m MockMessage) Release()                    {}
+func (m MockMessage) CommitAndRelease()           {}
