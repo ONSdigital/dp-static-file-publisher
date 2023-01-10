4f4521275e09992cc195a2b3a1a6af3571e56316
red <red54321@outlook.com>
2023-01-10 12:24:02 +0000

    Update version of lint, and version of golang used in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.17-stretch as build
+FROM golang:1.18-stretch as build
 
 WORKDIR /service
 ADD . /service
--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -4,6 +4,6 @@ cwd=$(pwd)
 
 pushd $cwd/dp-static-file-publisher
 # Install golangci-lint
-  go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.45.2
+  go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.50.1
   make lint
 popd
